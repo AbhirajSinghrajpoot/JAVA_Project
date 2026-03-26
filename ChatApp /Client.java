62e3d41617f82a66162c05964282bc36f7c66c6e
Abhiraj Singh Rajpoot <145330794+AbhirajSinghrajpoot@users.noreply.github.com>
2026-03-26 20:49:40 +0530

    Implement basic client for chat application

---
+++ ChatApp /Client.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 5000);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String msg = console.readLine();
+            out.println(msg);
+
+            String reply = in.readLine();
+            System.out.println("Server: " + reply);
+        }
+    }
+}
