09002c8c81e819e0988452ff41f95cf39e80f78b
Abhiraj Singh Rajpoot <145330794+AbhirajSinghrajpoot@users.noreply.github.com>
2026-03-26 20:49:05 +0530

    Update Server.java

--- ChatApp /Server.java
+++ ChatApp /Server.java
@@ -1,1 +1,24 @@
+import java.io.*;
+import java.net.*;
 
+public class Server {
+    public static void main(String[] args) throws Exception {
+        ServerSocket server = new ServerSocket(5000);
+        System.out.println("Server started... Waiting for client");
+
+        Socket socket = server.accept();
+        System.out.println("Client connected!");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String msg = in.readLine();
+            System.out.println("Client: " + msg);
+
+            String reply = console.readLine();
+            out.println(reply);
+        }
+    }
+}
